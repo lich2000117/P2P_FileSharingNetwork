638a5c6b0bc73facb350d7527ddf7da4e3fdb59a
Lee <lich2000117@gmail.com>
2022-08-27 21:52:49 +1000

    implement drop and upload file info to indxserver

--- src/main/java/comp90015/idxsrv/peer/Peer.java
+++ src/main/java/comp90015/idxsrv/peer/Peer.java
@@ -10,6 +10,7 @@ import java.util.ArrayList;
 import java.util.concurrent.LinkedBlockingDeque;
 
 import comp90015.idxsrv.filemgr.FileDescr;
+import comp90015.idxsrv.filemgr.FileMgr;
 import comp90015.idxsrv.message.*;
 import comp90015.idxsrv.server.IOThread;
 import comp90015.idxsrv.server.IndexElement;
@@ -67,19 +68,27 @@ public class Peer implements IPeer {
 		try{
 			RandomAccessFile raFile = new RandomAccessFile(file, "r");
 			FileDescr fd = new FileDescr(raFile);
+			String fileName = file.getName();
 			//send request
-			Message msgToSend = new ShareRequest(fd, file.getName(), shareSecret, idxPort);
+			Message msgToSend = new ShareRequest(fd, fileName, shareSecret, idxPort);
 			connection.sendRequest(msgToSend);
 			// receive reply
 			Message msg_back = connection.getMsg();
-			tgui.logInfo(msg_back.toString());
+			// check if it's error message
+			if (!checkReply(msg_back)){return;}
+			// if server accept, add to GUI
+			FileMgr fileMgr = new FileMgr(fileName, fd);
+			ShareReply reply = (ShareReply) msg_back;
+			ShareRecord newRecord = new ShareRecord(fileMgr, reply.numSharers," ", idxAddress,
+					idxPort, idxSecret, shareSecret);
+			tgui.addShareRecord(file.getName(), newRecord);
+			tgui.logInfo("shareFileWithIdxServer Finished!");
 		}
 		catch (Exception e) {
 			//e.printStackTrace();
 			tgui.logError("shareFileWithIdxServer Failed!");
 			return;
 		}
-		tgui.logInfo("shareFileWithIdxServer Finished!");
 	}
 
 	@Override
@@ -102,27 +111,28 @@ public class Peer implements IPeer {
 			connection.sendRequest(msgToSend);
 			// Add file info to GUI table
 			Message search_back = connection.getMsg();
-			if (search_back.getClass().getName() == SearchReply.class.getName()) {
-				SearchReply searchReply = (SearchReply) search_back;
-				tgui.logInfo("searchReply Received!");
-				IndexElement[] hits = searchReply.hits;
-				Integer[] seedCounts = searchReply.seedCounts;
-				// iterate through list of returned request (All relevant file lists + number of sharer for each file)
-				for (int i = 0; i < hits.length; i++) {
-					IndexElement ie = hits[i];
-					int curSeedCounts = seedCounts[i];
-					String fileName = ie.filename;
-					InetAddress inetAddress = InetAddress.getByName(ie.ip);
+			SearchReply searchReply = (SearchReply) search_back;
+			// check if it's error message
+			if (!checkReply(searchReply)){return;};
+			tgui.logInfo("searchReply Received!");
+			IndexElement[] hits = searchReply.hits;
+			Integer[] seedCounts = searchReply.seedCounts;
+			// before add, remove previous history
+			tgui.clearSearchHits();
+			// iterate through list of returned request (All relevant file lists + number of sharer for each file)
+			for (int i = 0; i < hits.length; i++) {
+				IndexElement ie = hits[i];
+				int curSeedCounts = seedCounts[i];
+				String fileName = ie.filename;
+				InetAddress inetAddress = InetAddress.getByName(ie.ip);
 
-					// create new searchrecord class and add to our gui table.
-					SearchRecord newSearchRecord =
-							new SearchRecord(ie.fileDescr, curSeedCounts, inetAddress, ie.port, idxSecret, ie.secret);
-					tgui.addSearchHit(fileName, newSearchRecord);
-					tgui.logInfo("searchRecord Added!");
-				}
+				// create new searchrecord class and add to our gui table.
+				SearchRecord newSearchRecord =
+						new SearchRecord(ie.fileDescr, curSeedCounts, inetAddress, ie.port, idxSecret, ie.secret);
+				tgui.addSearchHit(fileName, newSearchRecord);
+				tgui.logInfo("searchRecord Added!");
 			}
 		}
-
 		catch (Exception e) {
 			//e.printStackTrace();
 			tgui.logError("searchIdxServer Failed!");
@@ -133,13 +143,53 @@ public class Peer implements IPeer {
 
 	@Override
 	public boolean dropShareWithIdxServer(String relativePathname, ShareRecord shareRecord) {
-		tgui.logError("dropShareWithIdxServer unimplemented");
-		return false;
+
+		// try to establish connection and handshake with server.
+		ConnectServer connection = new ConnectServer(this.tgui);
+		if (!connection.MakeConnection(shareRecord.idxSrvAddress, shareRecord.idxSrvPort, shareRecord.idxSrvSecret)) {
+			tgui.logError("Connection Failed!");
+			return false;
+		}
+
+		// create and send request to share with server
+		try {
+			//send request
+			Message msgToSend = new DropShareRequest(relativePathname, shareRecord.fileMgr.getFileDescr().getFileMd5(),
+					shareRecord.sharerSecret, shareRecord.idxSrvPort);
+			connection.sendRequest(msgToSend);
+			// receive reply
+			Message msg_back = connection.getMsg();
+			// check if reply is a success flag to return false or true.
+			if (!checkReply(msg_back)){return false;}
+			DropShareReply dropShareReply = (DropShareReply) msg_back;
+			if (!dropShareReply.success) {
+				return false;
+			}
+			tgui.logInfo("Drop file success!");
+			return true;
+		}
+		catch (Exception e) {
+			//e.printStackTrace();
+			tgui.logError("Drop file Failed!");
+			return false;
+		}
 	}
 
 	@Override
 	public void downloadFromPeers(String relativePathname, SearchRecord searchRecord) {
 		tgui.logError("downloadFromPeers unimplemented");
+	}
+
+	/*
+	check the reply from server, if it's error message, return false, print to console.
+	Otherwise return true to indicate reply is valid.
+	 */
+	private boolean checkReply(Message msg_back){
+		if (msg_back.getClass().getName() == ErrorMsg.class.getName()) {
+			tgui.logError(((ErrorMsg) msg_back).msg);
+			return false;
+		}
+		return true;
 	}
 
 	/*
